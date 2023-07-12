5816a93e4d96007ac71208812100317089625c3b
eugeneleychenko <nycspicebo@gmail.com>
2023-07-12 11:01:37 -0400

    2

--- app.py
+++ app.py
@@ -1,129 +1,29 @@
+import streamlit as st
+import os
 from dotenv import find_dotenv, load_dotenv
 from langchain.chat_models import ChatOpenAI
-from langchain.vectorstores import FAISS
-from langchain.chains import RetrievalQA
-from langchain.document_loaders import TextLoader
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import OpenAI
+from langchain.agents import initialize_agent
+from langchain.agents.agent_toolkits import ZapierToolkit
+from langchain.agents import AgentType
+from langchain.utilities.zapier import ZapierNLAWrapper
 
-import os
-from trello import TrelloClient
-from bs4 import BeautifulSoup
-# from langchain.document import Document
+# Running dotenv
+load_dotenv(find_dotenv())
 
-API_KEY = os.getenv("API_KEY")
-TOKEN = os.getenv("TOKEN")
+zapier_nla_api_key=os.getenv("ZAPIER_NLA_API_KEY")
 openai_api_key=os.getenv("openai_api_key")
 
+llm = ChatOpenAI(temperature=0, openai_api_key=openai_api_key, model="gpt-3.5-turbo-16k")
+zapier = ZapierNLAWrapper(zapier_nla_api_key=zapier_nla_api_key)
+toolkit = ZapierToolkit.from_zapier_nla_wrapper(zapier)
+agent = initialize_agent(
+    toolkit.get_tools(), llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+)
 
-def get_trello_boards(client):
-    return client.list_boards()
+st.title('NLA Trello test')
 
-def get_trello_cards(board):
-    return board.list_cards()
-
-def get_list_from_card(board, card):
-    return board.get_list(card.list_id)
-
-def get_members_from_card(api_key, token, card_id):
-    return TrelloClient(api_key=api_key, token=token).get_card_members(card_id)
-
-def main():
-    api_key = API_KEY
-    token = TOKEN
-
-    client = TrelloClient(
-        api_key=api_key,
-        token=token
-    )
-
-    boards = get_trello_boards(client)
-    board_names = [board.name for board in boards]
-    ### hard coded board name
-    # selected_board_name = input('Enter a board name: ')
-    selected_board_name = "Smith Street Enterprises"
-
-    for board in boards:
-        if board.name == selected_board_name:
-            selected_board = board
-            break
-
-    cards = get_trello_cards(selected_board)
-    documents = []  # List to store Document objects
-    for card in cards:
-        text_content = ""
-        if card.name:
-            text_content = "Title: " + card.name + "\n"
-        if card.description.strip():
-            text_content += "Description: " + BeautifulSoup(card.description, "lxml").get_text() + "\n"
-        list = get_list_from_card(selected_board, card)
-        text_content += "List: " + list.name + "\n"
-        members = get_members_from_card(api_key, token, card.id)
-        text_content += "Members: " + ', '.join([member['fullName'] for member in members]) + "\n"
-        for checklist in card.checklists:
-            if checklist.items:
-                items = [
-                    f"{item['name']}:{item['state']}" for item in checklist.items
-                ]
-                text_content += "Checklist: " + checklist.name + "\n" + "\n".join(items) + "\n"
-        comments = [
-            BeautifulSoup(comment["data"]["text"], "lxml").get_text()
-            for comment in card.comments
-        ]
-        text_content += "Comments: " + "\n".join(comments) + "\n"
-        metadata = {
-            "title": card.name,
-            "id": card.id,
-            "url": card.url,
-        }
-        doc = Document(page_content=text_content, metadata=metadata)
-        documents.append(doc)  # Add the Document object to the list
-
-    return documents  # Return the list of Document objects
-
-def write_output_to_file(output):
-    # Open the file in write mode
-    file = open("output.txt", "w")
-
-    # Write each element of the list to the file
-    for item in output:
-        file.write(str(item) + "\n")
-
-    # Close the file
-    file.close()
-
-def run_query():
-    llm = ChatOpenAI(temperature=0, openai_api_key=openai_api_key, model="gpt-3.5-turbo")
-
-    loader = TextLoader('output.txt')
-    doc = loader.load()
-    print(f"You have {len(doc)} document")
-    print(f"You have {len(doc[0].page_content)} characters in that document")
-
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=3000, chunk_overlap=400)
-    docs = text_splitter.split_documents(doc)
-
-    # Get your embeddings engine ready
-    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-
-    # Embed your documents and combine with the raw text in a pseudo db. Note: This will make an API call to OpenAI
-    docsearch = FAISS.from_documents(docs, embeddings)
-
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=docsearch.as_retriever())
-
-    query = "Which projects have not yet been started?"
-    return qa.run(query)
-
-# Call the main function and store the result
-documents = main()
-
-# Now you can use the 'documents' variable in another section of your notebook
-output = [(doc.page_content, doc.metadata) for doc in documents]
-print(output)
-
-# Write the output to the file
-write_output_to_file(output)
-
-# Run the query and get the result
-query_result = run_query()
-print(query_result)
+input_text = st.text_input('Enter your command:')
+if st.button('Run'):
+    result = agent.run(input_text)
+    st.write(result)
